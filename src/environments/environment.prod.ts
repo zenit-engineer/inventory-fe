4f4d0bf6b84b6ba51b4c7c55261eccc1cd80de04
Zenk1 <neziri.zenit@gmail.com>
2025-01-06 13:28:41 +0100

    made a light change on ng.json for that property error when runing prod env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    backend_url: "https://stockmanagementbe-production.up.railway.app",
+    redirect_url: "http://localhost:4200",
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     backend_url: "http://localhost:8080",
     redirect_url: "http://localhost:4200",
 };
