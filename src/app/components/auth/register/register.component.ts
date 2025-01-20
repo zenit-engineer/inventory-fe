88962d4b4d3d2e06b6e5998ee40e222e890a3476
Zenk1 <neziri.zenit@gmail.com>
2025-01-20 16:47:54 +0100

    minor

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -85,10 +85,8 @@ export class RegisterComponent {
     this.authenticationSubscription = this.authService.register(postData as RegistrationRequest)
       .subscribe({
         next: (response) => {
-          debugger;
           if (response) {
             this.authResponse = response;
-            debugger;
             this.router.navigate(['activate-account'], 
               {
                state: { 
