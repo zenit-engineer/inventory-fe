3a26f236d494393d95a0033af38bef0dd737cbc2
Zenk1 <neziri.zenit@gmail.com>
2025-02-01 18:05:59 +0100

    added invoice component

--- src/app/components/base/base.routes.ts
+++ src/app/components/base/base.routes.ts
@@ -3,15 +3,15 @@ import { authGuard } from 'src/app/guard/auth.guard';
 
 export const baseRoutes: Routes = [
   {
-    path: 'statistics',
-    loadComponent: () =>
-      import('../statistics/statistics.component').then((m) => m.StatisticsComponent),
-    canActivate: [authGuard],
-  },
-  {
     path: 'home',
     loadComponent: () =>
       import('../home/home.component').then((m) => m.HomeComponent),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'statistics',
+    loadComponent: () =>
+      import('../statistics/statistics.component').then((m) => m.StatisticsComponent),
     canActivate: [authGuard],
   },
   {
@@ -20,4 +20,10 @@ export const baseRoutes: Routes = [
       import('../change-password/change-password.component').then((m) => m.ChangePasswordComponent),
     canActivate: [authGuard],
   },
+  {
+    path: 'invoice',
+    loadComponent: () =>
+      import('../invoice/invoice.component').then((m) => m.InvoiceComponent),
+    canActivate: [authGuard],
+  }
 ];
---
+++ src/app/components/invoice/invoice.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { CalendarModule } from 'primeng/calendar';
+import { DatePicker } from 'primeng/datepicker';
+
+@Component({
+  selector: 'app-invoice',
+  standalone: true,
+  imports: [
+    CommonModule,
+    InputNumberModule,
+    DropdownModule,
+    ReactiveFormsModule,
+    FormsModule,
+    InputTextModule,
+    FloatLabelModule,
+    CalendarModule,
+    DatePicker
+  ],
+  templateUrl: './invoice.component.html',
+  styleUrl: './invoice.component.scss'
+})
+export class InvoiceComponent {
+  constructor(
+    private fb: FormBuilder, 
+  ){}
+  categories:string[] = [];
+  suppliers:string[] = [];
+  manufacturers:string[] = [];
+  productForm = this.fb.group({
+    invoiceNumber: ["", Validators.required],
+  })
+}
